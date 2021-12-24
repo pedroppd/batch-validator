97447baf59c06820400a9d082a3bd9f7ad1af2d7
pedroppd <pedro_pdantas@hotmail.com>
2021-12-23 23:17:29 -0300

    Updates

--- src/main/java/br/com/oi/batchvalidation/BatchValidationApplication.java
+++ src/main/java/br/com/oi/batchvalidation/BatchValidationApplication.java
@@ -1,24 +1,12 @@
 package br.com.oi.batchvalidation;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@Slf4j
-@EnableJpaRepositories
-@EnableConfigurationProperties
-public class BatchValidationApplication implements CommandLineRunner {
+public class BatchValidationApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchValidationApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("Validation Started...");
 	}
 }
